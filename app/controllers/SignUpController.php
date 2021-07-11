7a83066d68c62669dbc374dffd9632ebe74ad8b6
mairo0510 <yamaguchi.takuya@his-world.com>
2021-07-11 19:11:23 +0900

    phalcon

---
+++ app/controllers/SignUpController.php
@@ -0,0 +1,41 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+class SignupController extends Controller
+{
+    public function indexAction()
+    {
+
+    }
+
+    public function registerAction()
+    {
+        $user = new Users();
+
+        //assign value from the form to $user
+        $user->assign(
+            $this->request->getPost(),
+            [
+                'name',
+                'email'
+            ]
+        );
+
+        // Store and check for errors
+        $success = $user->save();
+
+        // passing the result to the view
+        $this->view->success = $success;
+
+        if ($success) {
+            $message = "Thanks for registering!";
+        } else {
+            $message = "Sorry, the following problems were generated:<br>"
+                     . implode('<br>', $user->getMessages());
+        }
+
+        // passing a message to the view
+        $this->view->message = $message;
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,70 @@
+<?php
+
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Loader;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Application;
+use Phalcon\Url;
+use Phalcon\Db\Adapter\Pdo\Mysql;
+
+
+// Define some absolute path constants to aid in locating resources
+define('BASE_PATH', dirname(__DIR__));
+define('APP_PATH', BASE_PATH . '/app');
+// オートローダーの登録
+$loader = new Loader();
+
+$loader->registerDirs(
+    [
+        APP_PATH . '/controllers/',
+        APP_PATH . '/models/',
+        ]
+    );
+    
+$loader->register();
+
+$container = new FactoryDefault();
+
+$container->set(
+    'view',
+    function () {
+        $view = new View();
+        $view->setViewsDir(APP_PATH . '/views/');
+        return $view;
+    }
+);
+
+$container->set(
+    'url',
+    function () {
+        $url = new Url();
+        $url->setBaseUri('/');
+        return $url;
+    }
+);
+
+$application = new Application($container);
+
+$container->set(
+    'db',
+    function () {
+        return new Mysql(
+            [
+                'host'     => '127.0.0.1',
+                'username' => 'root',
+                'password' => 'mairo0510',
+                'dbname'   => 'tutorial',
+            ]
+        );
+    }
+);
+try {
+    // Handle the request
+    $response = $application->handle(
+        $_SERVER["REQUEST_URI"]
+    );
+    
+    $response->send();
+} catch (\Exception $e) {
+    echo 'Exception: ', $e->getMessage();
+}
